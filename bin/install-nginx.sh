f89859cf0d7b5f4855bcfd52faeedd0ceebbabe9
John Blackbourn <johnbillion@gmail.com>
2015-05-03 23:53:35 +0100

    Introduce Nginx for Travis tests.

---
+++ bin/install-nginx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PHP_VERSION=$(phpenv version-name)
+
+if [ $PHP_VERSION == '5.2' ]; then 
+	local CONF_NAME='nginx.52.conf'
+else
+	local CONF_NAME='nginx.conf'
+fi
+
+apt-get install nginx
+cp $CONF_NAME /etc/nginx/nginx.conf
+echo "127.0.0.1 example.org" | sudo tee -a /etc/hosts
+/etc/init.d/nginx restart
